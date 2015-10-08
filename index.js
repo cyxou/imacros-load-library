238588e7e524a9ead1976b54da50b45b79b87a70
cyxou <acyxou@gmail.com>
2015-10-08 12:58:12 +0300

    Add feature to clone object from window to context

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /* global Components */
+var clone = require('clone');
 
 try {
   Components.utils.importGlobalProperties(['XMLHttpRequest']);
@@ -12,10 +13,12 @@ try {
 /**
  * Gets the library from the provided url and evaluates it against specified context.
  *
- * @param  {string} url     Url to library's js file
- * @param  {object} context iMacros sandbox object
+ * @param  {string} url         Url to library's js file
+ * @param  {object} context     iMacros sandbox object
+ * @param  {string} objToClone  Optional object name provided by the loaded library
+ *                              to clone from window object to the context object.
  */
-module.exports = function(url, context) {
+module.exports = function(url, context, objToClone) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, false); // false stands for syncronous request
 
@@ -23,6 +26,9 @@ module.exports = function(url, context) {
     if (xhr.status >= 200 && xhr.readyState === 4) {
       eval.call(context, xhr.responseText);
       window.console.log('Script successfuly loaded from url: ' + url);
+
+      // clone object
+      context[objToClone] = clone(window[objToClone]);
     }
   });
 
